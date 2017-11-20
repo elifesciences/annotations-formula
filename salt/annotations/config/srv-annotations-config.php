6862f53bbd28018f3984fcc718041658429c8c54
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-20 13:36:17 +0000

    Providing a full config.php
    The file's format now accomodates all the Hypothesis configuration, plus some application-wide ones like logging

--- salt/annotations/config/srv-annotations-config.php
+++ salt/annotations/config/srv-annotations-config.php
@@ -1,7 +1,25 @@
 <?php
+
+use Psr\Log\LogLevel;
+
 return [
-    'secret' => '{{ pillar.annotations.secret }}',
-    'hypothesis_api_url' => '{{ pillar.annotations.hypothesis_api_url }}',
-    'hypothesis_api_publisher' => '{{ pillar.annotations.hypothesis_api_publisher }}',
-    'ttl' => {{ pillar.annotations.ttl }},
+    'debug' => false,
+    'logging' => [
+        'level' => LogLevel::{{ pillar.annotations.logging.level }},
+    ],
+    'hypothesis' => [
+        'api_url' => '{{ pillar.annotations.hypothesis.api_url }}',
+        'client_id' => '{{ pillar.annotations.hypothesis.client_id }}',
+        'secret_key' => '{{ pillar.annotations.hypothesis.secret_key }}',
+        'authority' => '{{ pillar.annotations.hypothesis.authority }}',
+    ],
+    'aws' => [
+        'queue_name' => 'annotations--{{ pillar.elife.env }}',
+        'queue_message_default_type' => 'profiles',
+        'credential_file' => true,
+        'region' => '{{ pillar.elife.aws.region }}',
+        {% if pillar.elife.env in ['dev', 'ci'] -%}
+        'endpoint' => 'http://localhost:4100',
+        {%- endif %}
+    ],
 ];
