4b226fb2da4e9b5ac765055b56b203e45311802b
Luke <2142748+lsh-0@users.noreply.github.com>
2024-03-15 14:49:14 +1030

    smoketests, adds pause before testing ping

--- salt/annotations/config/srv-annotations-smoke_tests.sh
+++ salt/annotations/config/srv-annotations-smoke_tests.sh
@@ -6,6 +6,10 @@ cd /home/{{ pillar.elife.deploy_user.username }}/annotations
 docker-compose exec queue_watch ./smoke_tests_cli.sh
 docker-compose exec fpm ./smoke_tests_fpm.sh
 
+# annotations doesn't have a healthcheck right now but the slight pause helps
+timeout=5 # seconds
+docker-wait-healthy annotations_fpm_1 $timeout || true
+
 set +e
 smoke_url_ok localhost/ping
 smoke_report
